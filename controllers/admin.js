bb5ae09fca6382b454b27623950a6dd2b0ea7626
av <vihrogonov@protonmail.com>
2020-12-14 23:21:37 +0000

    Displaying Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,8 +13,12 @@ exports.getAddProduct = (req, res, next) => {
 
 // Render Add Product
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title); // create new product based on the class
-  product.save(); // save product
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save(); 
   res.redirect('/');
 };
 
