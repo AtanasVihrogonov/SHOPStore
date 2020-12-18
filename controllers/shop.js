ae3ae8414ded6fda926323efa04c525600f1eb3e
av <vihrogonov@protonmail.com>
2020-12-18 12:19:40 +0000

    Editing & Deleting Products

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,6 +29,15 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart',
   });
 };
+
+// Render views/ Orders Page
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders',
+  });
+};
+
 // Render views/ Checkout Page
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
