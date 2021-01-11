0717626169e61d063b91b1df37e27597d4ce427e
av <vihrogonov@protonmail.com>
2021-01-11 17:01:53 +0000

    Adding a Cart Model and Using Query Params

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,9 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 // Render views/ Products Page
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
@@ -14,19 +15,18 @@ exports.getProducts = (req, res, next) => {
 // Render views/ Product Page
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId, (product) => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
-      path: '/products'
-    })
-  })
-  
-}
+      path: '/products',
+    });
+  });
+};
 
 // Render views/ Index Page
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
@@ -45,9 +45,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId)
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
-}
+};
 
 // Render views/ Orders Page
 exports.getOrders = (req, res, next) => {
