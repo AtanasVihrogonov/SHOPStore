f1e7185fa905927a806d24158e9768900a354423
av <vihrogonov@protonmail.com>
2021-01-20 20:14:51 +0000

    Session & Cookies

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://av001:Go849114Akv@av001.yvvhm.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,6 +27,16 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session(
+    {
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store,
+    }
+  )
+);
 
 app.use((req, res, next) => {
   User.findById('6004c93207572608cd648865')
@@ -35,18 +54,16 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://av001:Go849114Akv@av001.yvvhm.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then(user => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
           name: 'John',
           email: 'john@test.com',
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
