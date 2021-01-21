01ce7fd25806203b9cf1c9b329033e4af12fddaf
av <vihrogonov@protonmail.com>
2021-01-21 15:57:20 +0000

    Using Middleware to Protect Routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,12 +20,29 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('6004c93207572608cd648865')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      console.log(err);
-      res.redirect('/');
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch((err) => {
+          res.redirect('/login');
+        });
     })
     .catch((err) => console.log(err));
 };
