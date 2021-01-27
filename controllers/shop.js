7f8734a08af910f2bea7900087bc2088c9876297
av <vihrogonov@protonmail.com>
2021-01-27 16:23:39 +0000

    adding Pagination Logic & Controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,17 +6,34 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 3;
+
 // @ GET
 // @ getProducts
 //-------------------
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
+        pageTitle: 'Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -50,12 +67,28 @@ exports.getProduct = (req, res, next) => {
 // @ getIndex
 //-------------------
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then((products) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch((err) => {
@@ -209,16 +242,18 @@ exports.getInvoice = (req, res, next) => {
       order.products.forEach((prod) => {
         // totalPrice = totalPrice + prod.quantity * prod.product.price
         totalPrice += prod.quantity * prod.product.price;
-        pdfDoc.fontSize(14).text(
-          prod.product.title +
-            ' - ' +
-            prod.quantity +
-            ' x ' +
-            '£' +
-            prod.product.price
-        );
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              ' - ' +
+              prod.quantity +
+              ' x ' +
+              '£' +
+              prod.product.price
+          );
       });
-      pdfDoc.text('---')
+      pdfDoc.text('---');
       pdfDoc.fontSize(20).text('Total Price: £' + totalPrice);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
